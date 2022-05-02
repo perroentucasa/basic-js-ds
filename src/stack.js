b97dba5bc3675f40aabcc01df5793da7903c788a
perroentucasa <86454571+perroentucasa@users.noreply.github.com>
2022-05-02 21:29:46 +0300

    feat: stack solution

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
   }
-
+  push(e) {
+    this.stack.push(e);
+  }
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop();
   }
-
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
